c63dd2b304600c030d35e929b688658d435415d3
Jenil J Gandhi <63243203+jenilgandhi2111@users.noreply.github.com>
2022-01-19 21:30:49 +0530

    Minor changes

---
+++ Client/src/menu-items/Student/AnnouncementStudentMenu.js
@@ -0,0 +1,41 @@
+// assets
+import { IconDashboard, IconDeviceAnalytics, IconSpeakerphone, IconLayoutGridAdd, IconCirclePlus, IconEye, IconSportBillard, IconBellRinging } from '@tabler/icons';
+
+// constant
+const icons = {
+    IconDashboard: IconDashboard,
+    IconDeviceAnalytics,
+    IconEye,
+    IconSpeakerphone,
+    IconLayoutGridAdd,
+    IconCirclePlus,
+    IconSportBillard, IconBellRinging
+
+};
+
+//-----------------------|| ANNOUNCEMENT DASHBOARD MENU ITEMS ||-----------------------//
+
+export const AnnouncementStudentMenu = {
+    id: 'AnnouncementStudentMenu',
+    title: 'Announcements',
+    type: 'group',
+    children: [
+        {
+            id: 'view_announcements',
+            title: 'View Announcements',
+            type: 'item',
+            url: '/_student/announcement/view_announcement',
+            icon: icons['IconEye'],
+            breadcrumbs: false
+        },
+        {
+            id: 'subscribed_announcements',
+            title: 'Subscribed Announcements',
+            type: 'item',
+            url: '/_student/announcement/subscribed_announcements',
+            icon: icons['IconBellRinging'],
+            breadcrumbs: false
+        },
+
+    ]
+};
