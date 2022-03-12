4517efa2192e8034509202cdd5f3955d05da647b
Jenil J Gandhi <63243203+jenilgandhi2111@users.noreply.github.com>
2022-03-12 11:22:56 +0530

    Models updated

--- Server/Models/index.js
+++ Server/Models/index.js
@@ -37,7 +37,7 @@ db.student_projects = require("./StudentProjectModel")(sequelize, DataTypes)
 db.student_achievements_internships = require(".//StudentAchievementsInternshipsModel")(sequelize, DataTypes)
 
 
-db.sequelize.sync({ force: true }).then(() => {
+db.sequelize.sync({ force: false }).then(() => {
     console.log("Resyncing Done.....")
 })
 
