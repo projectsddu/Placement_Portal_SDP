8953cc8399cc1e20d946d97a2f57f765c27c958f
Jenil J Gandhi <63243203+jenilgandhi2111@users.noreply.github.com>
2022-08-17 17:41:04 +0530

    Testing commit

--- Scripts/Test.sh
+++ Scripts/Test.sh
@@ -1,8 +1,15 @@
-ls
-VERSION=`ls -td -- */ | head -n 1 | cut -d'/' -f1`
-echo $VERSION
-VERSION="${VERSION:1:2}"
-VERSION="$((VERSION+1))"
-echo $VERSION
-mkdir "V"${VERSION}
-read ip
+# ls
+# VERSION=`ls -td -- */ | head -n 1 | cut -d'/' -f1`
+# echo $VERSION
+# VERSION="${VERSION:1:2}"
+# VERSION="$((VERSION+1))"
+# echo $VERSION
+# mkdir "V"${VERSION}
+# read ip
+
+cd ..
+while true;
+do
+git status
+sleep 1;
+done
